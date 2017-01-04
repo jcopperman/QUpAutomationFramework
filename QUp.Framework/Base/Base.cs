525f15f5336442996529ffbf710ad38bfc9ec1e1
Jonathan Opperman <Jonathan Opperman>
2017-01-04 09:23:53 +0200

    Initial commit

---
+++ QUp.Framework/Base/Base.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QUp.Framework.Base;
+
+namespace QUp.Framework.Base
+{
+    public class Base
+    {
+        public BasePage CurrentPage { get; set; }
+
+        private IWebDriver _driver { get; set; }
+
+        /// <summary>
+        /// Using Generics to avoid boxing and unboxing of the Selenium Webdriver
+        /// </summary>
+        /// <typeparam name="TPage"></typeparam>
+        /// <returns></returns>
+        protected TPage GetInstance<TPage>() where TPage : BasePage, new()
+        {
+            TPage pageInstance = new TPage()
+            {
+                _driver = DriverContext.Driver
+            };
+
+            PageFactory.InitElements(DriverContext.Driver, pageInstance);
+
+            return pageInstance;
+        }
+
+        public TPage As<TPage>() where TPage : BasePage
+        {
+            return (TPage)this;
+        }
+    }
+}
---
+++ QUp.Framework/Helpers/LogHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace QUp.Framework.Helpers
+{
+    public class LogHelpers
+    {
+        private static string _logFileName = string.Format("{0:yyyymmddhhmmss}", DateTime.Now);
+        private static StreamWriter _streamw = null;
+
+        //Create log file 
+        public static void CreateLogFile()
+        {
+            string dir = @"C:\Dev\QUp.Automation.Framework\";
+            if(Directory.Exists(dir))
+            {
+                _streamw = File.AppendText(dir + _logFileName + ".log");
+            }
+            else
+            {
+                Directory.CreateDirectory(dir);
+                _streamw = File.AppendText(dir + _logFileName + ".log");
+            }
+        }
+
+        //Streamwriter write method to log date and log message to file
+        public static void Write(string logMessage)
+        {
+            _streamw.Write("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+            _streamw.WriteLine("    {0}", logMessage);
+            _streamw.Flush();
+        }
+    }
+}
